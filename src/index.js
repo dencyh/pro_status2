c3e6866509d80fc08a6ebcc0e68d74989b47ebbc
Alexander Moryan <a.dencyh@gmail.com>
2022-07-13 13:13:13 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,80 +2,43 @@ const puppeteer = require('puppeteer');
 const XLSX = require("xlsx");
 const pages = require('./pages');
 const companies = require('./companies');
+const parks = require('./parks');
 
 
-(async function scrape() {
+const start = async function scrape() {
     const browser = await puppeteer.launch({
         headless: false,
         userDataDir: "~/Library/Application Support/Google/Chrome",
-        args: ['--lang=ru-Ru,ru']
+        LANGUAGE: "ru_Ru"
     });
     const page = await browser.newPage();
-    await page.goto(pages["Проект А"][0]);
-    await page.waitForSelector('.rc-pagination-item');
+
 
     let list = []
-    for (let companyListItem in pages) {
-        for (let i = 0; i < pages[companyListItem].length; i++) {
 
-            await page.goto(pages[companyListItem][i]);
-            console.log(pages[companyListItem][i])
-            await page.waitForSelector('.DriverLink_container__35QA8');
+    for (let park in parks) {
+        let initialUrl = `https://fleet.taxi.yandex.ru/drivers?status=working&page=1&limit=100&park_id=${parks[park]}`
+        await page.goto(initialUrl);
+        await page.waitForSelector('.rc-pagination-item');
+
+        let pagesNums = await page.evaluate(() => {
+            return Array.from(document.body.querySelectorAll('.rc-pagination-item')).length
+        });
+
+
+        for (let i = 1; i <= pagesNums; i++) {
+
+            let url = `https://fleet.taxi.yandex.ru/drivers?status=working&page=${i}&limit=100&park_id=${parks[park]}`;
+            await page.goto(url);
+            await page.waitForSelector('.Table_name__-P4ig .DriverLink_container__35QA8');
 
             /* Main logic*/
-            let driversList = await page.evaluate(() => {
-                let drivers = [];
-                /* Status */
-                let status = Array.from(document.querySelectorAll(".Table_status__3tCyu .DriverStatusCell_badge__BtkR_"))
-                    .map(element => element.innerText)
-
-                /* Callsign */
-                let callsign = Array.from(document.querySelectorAll(".Table_callSign__jOAzL"))
-                    .slice(1, 101)
-                    .map(element => element.innerText)
-
-                /* Names */
-                let names = Array.from(document.querySelectorAll(".Table_name__-P4ig .DriverLink_container__35QA8"))
-                    .map(element => element.innerText)
-                    .map((name) => name.slice(0, name.length - 1));
-                let namesShort = names
-                    .map((name) => name.split(" "))
-                    .map((name) => name[0] + " " + name[1]);
-
-                /* Phones */
-                let phones = Array.from(document.querySelectorAll('td.Text_typography_caption:nth-child(5n)'))
-                    .filter((element, index) => index % 2 === 0)
-                    .map(element => element.innerText);
-                /* Plate numbers */
-                let plateNumbers = Array.from(document.querySelectorAll('td.Text_typography_caption:nth-child(5n)'))
-                    .filter((element, index) => index % 2 !== 0)
-                    .map(element => element.innerText.replace('Ford Transit', ''));
-                /* Park */
-                let companyRaw = document.querySelector(".Text.Text_overflow_ellipsis.Text_typography_body").innerText;
-                /*let company = companies[companyRaw]*/
-
-                /*   Pushing data to object */
-                status.forEach((status, index) => {
-                    drivers.push({
-                        status: status,
-                        callsign: callsign[index],
-                        name: names[index],
-                        nameShort: namesShort[index],
-                        phone: phones[index],
-                        plateNumbers: plateNumbers[index],
-                        company: companyRaw,
-                    });
-                });
-
-                return drivers
-            })
+            let driversList = await page.evaluate(scanSinglePage);
+            console.log('driversList', driversList)
 
             list = [...list, ...driversList]
-
-
         }
     }
-
 
     const correspondingStatus = {
         'Available': 'Свободный',
@@ -83,7 +46,6 @@ const companies = require('./companies');
         'En route': 'На заказе',
         'Offline': 'Офлайн'
     }
-
     list.forEach((item) => {
         if (companies[item.company]) {
             item.company = companies[item.company]
@@ -109,4 +71,57 @@ const companies = require('./companies');
     XLSX.writeFile(newWB, "./ProStatus.xlsx");
 
     await browser.close();
-})();
+
+};
+
+start();
+
+
+/* Helper functions */
+function scanSinglePage() {
+
+    let drivers = [];
+    /* Status */
+    let status = Array.from(document.querySelectorAll(".Table_status__3tCyu .DriverStatusCell_badge__BtkR_"))
+        .map(element => element.innerText)
+
+    /* Callsign */
+    let callsign = Array.from(document.querySelectorAll(".Table_callSign__jOAzL"))
+        .slice(1, 101)
+        .map(element => element.innerText)
+
+    /* Names */
+    let names = Array.from(document.querySelectorAll(".Table_name__-P4ig .DriverLink_container__35QA8"))
+        .map(element => element.innerText)
+        .map((name) => name.slice(0, name.length - 1));
+    let namesShort = names
+        .map((name) => name.split(" "))
+        .map((name) => name[0] + " " + name[1]);
+
+    /* Phones */
+    let phones = Array.from(document.querySelectorAll('td.Text_typography_caption:nth-child(5n)'))
+        .filter((element, index) => index % 2 === 0)
+        .map(element => element.innerText);
+    /* Plate numbers */
+    let plateNumbers = Array.from(document.querySelectorAll('td.Text_typography_caption:nth-child(5n)'))
+        .filter((element, index) => index % 2 !== 0)
+        .map(element => element.innerText.replace('Ford Transit', ''));
+    /* Park */
+    let companyRaw = document.querySelector(".Text.Text_overflow_ellipsis.Text_typography_body").innerText;
+    /*let company = companies[companyRaw]*/
+
+    /*   Pushing data to object */
+    status.forEach((status, index) => {
+        drivers.push({
+            status: status,
+            callsign: callsign[index],
+            name: names[index],
+            nameShort: namesShort[index],
+            phone: phones[index],
+            plateNumbers: plateNumbers[index],
+            company: companyRaw,
+        });
+    });
+    console.log('drivers', drivers)
+    return drivers
+}
